a158531a5df2609b043f0505cf947483ee257ec9
Jonathan Brouwer <jonathantbrouwer@gmail.com>
2026-09-06 21:06:01 +0200

    Add a regression test for trailing attributes in doctests

---
+++ tests/rustdoc-ui/expect-item-after-attribute.rs
@@ -0,0 +1,26 @@
+//@ compile-flags: --test --test-args=--test-threads=1
+//@ normalize-stdout: "finished in \d+\.\d+s" -> "finished in $$TIME"
+//@ failure-status: 101
+
+//! ```
+//! #[should_panic]
+//! ```
+
+//! ```
+//! fn main() {
+//!     #[should_panic]
+//! }
+//! ```
+
+//! ```
+//! fn main() { }
+//! #[should_panic]
+//! ```
+
+//! ```
+//! let x = 0; #[should_panic]
+//! ```
+
+//! ```
+//! let x = 0; //! assert!(true);
+//! ```
