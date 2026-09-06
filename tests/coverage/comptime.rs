0027ee161223389c9490ffef6d07f1cc00a75385
Zalathar <Zalathar@users.noreply.github.com>
2026-09-06 16:47:24 +1000

    Make comptime functions ineligible for coverage
    Compile-time-only functions don't generate code, so instrumenting them for
    coverage is useless.
    
    This also avoids an ICE when trying to get the function's symbol name for an
    unused-function record, which can occur when instrumenting `core`.
    
    Co-Authored-By: Rachel Barker <rachel.barker@ferrous-systems.com>

--- tests/coverage/comptime.rs
+++ tests/coverage/comptime.rs
@@ -1,7 +1,5 @@
 #![feature(rustc_attrs)]
 //@ edition: 2024
-//@ compile-flags: -Cinstrument-coverage
-//@ needs-profiler-runtime
 
 // Check that instrumenting a crate with a comptime function doesn't ICE.
 // (The function itself doesn't need to be instrumented, and probably shouldn't be.)
